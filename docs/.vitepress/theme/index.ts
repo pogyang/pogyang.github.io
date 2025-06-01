0ca19f55c04634eff617fcdd8b88fd64113abd51
pogyang <im.pogyang@outlook.com>
2025-06-01 23:56:41 +0800

    0

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,14 @@
 // import DefaultTheme from 'vitepress/theme'
 import Layout from './Layout.vue'
+import 'viewerjs/dist/viewer.min.css'
+import imageViewer from 'vitepress-plugin-image-viewer'
+import { useRoute } from 'vitepress'
 import './custom.css'
 
 export default {
-  Layout
+  Layout,
+  setup() {
+    const route = useRoute()
+    imageViewer(route)
+  },
 }
